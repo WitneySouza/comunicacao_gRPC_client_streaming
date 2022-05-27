0dc4c7ab901982ef6060449b79d4a3972108bd18
witney <valentinareinande@gmail.com>
2022-05-26 22:32:42 -0300

    Commit one

---
+++ src/main/java/br/edu/ifg/calculator/CalculatorClient.java
@@ -0,0 +1,34 @@
+package br.edu.ifg.calculator;
+
+import br.edu.ifg.proto.calculator.CalculatorServiceGrpc;
+import br.edu.ifg.proto.calculator.SumRequest;
+import br.edu.ifg.proto.calculator.SumResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+public class CalculatorClient {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a gRPC client");
+        CalculatorClient main = new CalculatorClient();
+        main.run();
+    }
+    private void run() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",
+                        50052)
+                .usePlaintext()
+                .build();
+        doUnaryCall(channel);
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+    private void doUnaryCall(ManagedChannel channel){
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub stub =
+                CalculatorServiceGrpc.newBlockingStub(channel);
+        SumRequest request = SumRequest.newBuilder()
+                .setFirstNumber(11)
+                .setSecondNumber(50)
+                .build();
+        SumResponse response = stub.sum(request);
+        System.out.println(request.getFirstNumber() + " + " +
+                request.getSecondNumber() + " = " + response.getSumResult());
+    }
+}
